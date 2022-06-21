feea6b018b9f431e39a14d4a74e33fc083407384
Shelldin <sheldon.bean@gmail.com>
2022-06-20 19:44:12 -0600

    RL Tut: BoardManager Script

---
+++ UnityTutorials/Roguelike Tutorial/Unity Roguelike Tutorial/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int columns = 8;
+    public int rows = 8;
+
+    public Count wallCount = new Count(4, 9);
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
