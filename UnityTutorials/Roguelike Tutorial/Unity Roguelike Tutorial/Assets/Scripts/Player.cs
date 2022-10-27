900036c16e0b371d166959bb9b2ffcde2a510dde
Shelldin <sheldon.bean@gmail.com>
2022-10-27 14:19:32 -0600

    RL tut: Added touch controls and finished tutorial

--- UnityTutorials/Roguelike Tutorial/Unity Roguelike Tutorial/Assets/Scripts/Player.cs
+++ UnityTutorials/Roguelike Tutorial/Unity Roguelike Tutorial/Assets/Scripts/Player.cs
@@ -20,6 +20,8 @@ public class Player : MovingObject
 
     private int food;
 
+    private Vector2 touchOrigin = -Vector2.one;
+
     public AudioClip moveSound1,
         moveSound2,
         eatSound1,
@@ -77,6 +79,9 @@ public class Player : MovingObject
         int horizontal = 0;
         int vertical = 0;
 
+
+
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
         horizontal = (int) Input.GetAxisRaw("Horizontal");
         vertical = (int) Input.GetAxisRaw("Vertical");
 
@@ -84,6 +89,34 @@ public class Player : MovingObject
         {
             vertical = 0;
         }
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    horizontal = x > 0 ? 1 : -1;
+                }
+                else
+                {
+                    vertical = y > 0 ? 1 : -1;
+                }
+            }
+        }
+        
+#endif
 
         if (horizontal !=0 || vertical !=0)
         {
