582326c37d645912ed61bb87ac7cce2d4b8fb5a3
Shelldin <sheldon.bean@gmail.com>
2022-11-03 14:58:42 -0600

    TBS: set up AI navmesh for character movement

--- TBS Tutorial/Assets/Scripts/CharacterController.cs
+++ TBS Tutorial/Assets/Scripts/CharacterController.cs
@@ -1,34 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class CharacterController : MonoBehaviour
 {
     public float moveSpeed;
     private Vector3 moveTarget;
 
+    public NavMeshAgent navAgent;
+    private bool isMoving;
+
+    public bool isEnemy;
+
 
     void Start()
     {
+        navAgent = GetComponent<NavMeshAgent>();
         moveTarget = transform.position;
+
+        navAgent.speed = moveSpeed;
     }
 
     void Update()
     {
         //move towards a target point
-        if (transform.position != moveTarget)
+        if (isMoving)
         {
             
-            transform.position = Vector3.MoveTowards(transform.position, moveTarget, moveSpeed * Time.deltaTime);
+            //transform.position = Vector3.MoveTowards(transform.position, moveTarget, moveSpeed * Time.deltaTime);
             
             //check if current unit/player is the active unit
             if (GameManager.instance.activePlayer == this)
             {
+                //if current player is the active player move Camera to view that player
+                CameraController.instance.SetMoveTarget(transform.position);
+                
                 //check to prevent minor misalignment error when switching players
-                if (Vector3.Distance(transform.position, moveTarget) > .1f)
+                if (Vector3.Distance(transform.position, moveTarget) < .2f)
                 {
-                    //if current player is the active player move Camera to view that player
-                    CameraController.instance.SetMoveTarget(transform.position);
+                    isMoving = false;
                 }
             }
         }
@@ -37,5 +49,8 @@ public class CharacterController : MonoBehaviour
     public void MoveToPoint(Vector3 movePoint)
     {
         moveTarget = movePoint;
+
+        navAgent.SetDestination(moveTarget);
+        isMoving = true;
     }
 }
